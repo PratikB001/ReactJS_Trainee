84c7de99f0db694a49c95865598e945d5eb76f04
Pratik Budhathoki <nischalbudhathoki99@gmail.com>
2022-07-06 20:46:25 +0545

    Final Commit

--- src/components/expenses/ExpenseItems.js
+++ src/components/expenses/ExpenseItems.js
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItems.css";
 import ExpenseDate from "../expenses/ExpenseDate";
 
 const ExpenseItems = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
-
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2> {title}</h2>
+        <h2> {props.title}</h2>
         <div className='expense-item__price'> ${props.amount}</div>
       </div>
-      <button onClick={clickHandler}> Change Title</button>
     </div>
   );
 };
